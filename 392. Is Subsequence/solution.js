8e72fa867769dc543bd3e902ffa37572c9e9abce
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-02-19 23:16:40 -0800

    isSubsequence  solution

---
+++ 392. Is Subsequence/solution.js
@@ -0,0 +1,9 @@
+var isSubsequence = function (s, t) {
+    let x = 0;
+    let y = 0;
+    for (y = 0; y < t.length && x < s.length; y++)
+        if (t[y] === s[x]) {
+            x++
+        }
+    return (s.length === x)
+};
