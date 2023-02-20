4f84960dd9cae6a8aa53b32cc83d5ee8d3d48873
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-02-19 22:23:29 -0800

    isIsomorphic solution has been added.

--- 205. Isomorphic Strings/solution.js
+++ 205. Isomorphic Strings/solution.js
@@ -2,4 +2,21 @@ var isIsomorphic = function (s, t) {
     if (s.length !== t.length) {
         return false
     }
+    let sMap = {};
+    let tMap = {};
+    for (let i = 0; i < s.length; i++) {
+        let currentSChar = s[i];
+        let currentTChar = t[i];
+
+        if (!sMap[currentSChar]) {
+            sMap[currentSChar] = currentTChar
+        }
+        if (!tMap[currentTChar]) {
+            tMap[currentTChar] = currentSChar
+        }
+        if (sMap[currentSChar] != currentTChar || tMap[currentTChar] != currentSChar) {
+            return false
+        }
+    }
+    return true
 };
